f2104b3f22d64254928e2ef36d391aa4b9c9f723
Soresu <g_mester@freemail.hu>
2015-04-29 19:20:19 +0200

    Throw with button, Enemy filer
    Some changes to fit to my playstyle. Option to don't trow selected champions(like alistar), Autothrow can be disabled and throw with button.

--- ARAMShooter/Program.cs
+++ ARAMShooter/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
@@ -32,6 +32,14 @@ namespace ARAMShooter
                     new StringList(
                         new[] { HitChance.Low.ToString(), HitChance.Medium.ToString(), HitChance.High.ToString() }, 1)));
             Menu.AddItem(new MenuItem("Auto", "AutoDash").SetValue(true));
+            Menu.AddItem(new MenuItem("AutoThrow", "AutoThrow").SetValue(false));
+            Menu.AddItem(new MenuItem("Throw", "Throw").SetValue(new KeyBind("T".ToCharArray()[0], KeyBindType.Press)));
+            var dontThrow = new Menu("Don't throw at", "DontThrow");
+            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy))
+            {
+                dontThrow.AddItem(new MenuItem("DontThrow" + hero.SkinName, hero.SkinName)).SetValue(false);
+            }
+            Menu.AddSubMenu(dontThrow);
             Menu.Item("HitChance").ValueChanged += Program_ValueChanged;
             Menu.Item("DecreaseRange").ValueChanged += Program_ValueChanged1;
             Menu.AddToMainMenu();
@@ -45,6 +53,10 @@ namespace ARAMShooter
 
         private static void Game_OnUpdate(EventArgs args)
         {
+            if (Menu.Item("Throw").GetValue<KeyBind>().Active)
+            {
+                ObjectManager.Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+            }
             if (!Throw.IsReady())
             {
                 return;
@@ -60,11 +72,15 @@ namespace ARAMShooter
                 Throw.Cast();
                 return;
             }
-
-            foreach (var champ in
-                ObjectManager.Get<Obj_AI_Hero>().Where(h => h.IsValidTarget(Throw.Range)))
+            if (Menu.Item("Throw").GetValue<KeyBind>().Active || Menu.Item("AutoThrow").GetValue<bool>())
             {
-                Throw.Cast(champ);
+                foreach (var champ in
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .Where(
+                            h => h.IsValidTarget(Throw.Range) && !Menu.Item("DontThrow" + h.SkinName).GetValue<bool>()))
+                {
+                    Throw.Cast(champ);
+                }
             }
         }
 
